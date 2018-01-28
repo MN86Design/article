8aee84fcd000fe9f5ed5ccf7f8f3d3892c462fad
MadokaOffice <m.nakazawa.office@gmail.com>
2018-01-29 05:49:14 +0900

    Initial Commit

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="icon" href="favicon.ico">
+  <title>Dotinstall Pane</title>
+</head>
+<body>
+  <header>
+    <div class="container">
+      <p>
+        3分動画を参考に、自ら手を動かすことで<br>
+        気軽にプログラミング学習できるサービス
+      </p>
+      <h1>Dotinstall Pane</h1>
+      <a class="btn" href="#">詳細を見る <i class="fa fa-external-link" aria-hidden="true"></i></a>
+      <img src="img/top.png" width="480" height="270" alt="Atomエディタ" class="shadow">
+    </div>
+  </header>
+
+  <section class="features">
+    <h2>Dotinstall Paneの特徴</h2>
+    <div class="container">
+      <div class="row">
+        <img src="img/feature1.png" width="420" height="270" alt="Dotinstall Pane" class="shadow">
+        <div class="desc pull-right">
+          <h3>Atomは使いやすい！</h3>
+          <p>無料のテキストエディタの中では、ハイスペックで使いやすいAtomを使って、一緒に学習できます！</p>
+        </div>
+      </div>
+      <div class="row">
+        <img src="img/feature2.png" width="420" height="270" alt="Dotinstall Pane" class="shadow">
+        <div class="desc pull-left">
+          <h3>ソースコードを比較学習できる！</h3>
+          <p>独学だと壁にぶつかって時間を費やしてしまいがちです。ドットインストールでは、ソース例が確認でき、自分のソースと比較できます。</p>
+        </div>
+      </div>
+      <div class="row">
+        <img src="img/feature3.png" width="420" height="270" alt="Dotinstall Pane" class="shadow">
+        <div class="desc pull-right">
+          <h3>何でも動画で学習できる！</h3>
+          <p>インプットしながら、並行してアウトプットできるのがドットインストールの強みです。何度でも反復学習できるのが魅力です。</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="voices">
+    <h2>私の感想</h2>
+    <div class="container">
+      <div class="user">
+        <img src="img/icon1.jpg" width="100" height="100" alt="魅力その１">
+        <h3>魅力その１</h3>
+        <p>ローカル開発環境の構築方法もしっかり動画で確認できる！</p>
+      </div>
+      <div class="user">
+        <img src="img/icon2.jpg" width="100" height="100" alt="魅力その２">
+        <h3>魅力その２</h3>
+        <p>動画の内容が文章で読めるので、聞き漏れが防止できる！</p>
+      </div>
+      <div class="user">
+        <img src="img/icon3.jpg" width="100" height="100" alt="魅力その３">
+        <h3>魅力その３</h3>
+        <p>様々な言語の学習レッスンが、どんどんリリースされる！</p>
+      </div>
+    </div>
+  </section>
+
+  <section class="call-to-action">
+    <h2>Dotinstall Paneで、プログラミング学習をもっと身近に！</h2>
+    <div class="container">
+      <a class="btn" href="#">詳細を見る <i class="fa fa-external-link" aria-hidden="true"></i></a>
+    </div>
+  </section>
+
+  <footer>
+    <p>dotinstall.com</p>
+  </footer>
+
+</body>
+</html>
